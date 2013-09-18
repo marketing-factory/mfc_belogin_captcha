5b056832698f990c4c59c05d8f7d53721cc91ba2
sfischer <sfischer@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-09-18 13:00:29 +0000

    Initial import
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mfc_belogin_captcha/trunk@79873 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "tmpl_dev".
+#
+# Auto generated 02-05-2011 06:33
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Backend Login Captcha',
+	'description' => 'Template storage',
+	'category' => 'be',
+	'author' => 'Sebastian Fischer',
+	'author_email' => 'typo3@marketing-factory.de',
+	'shy' => '',
+	'conflicts' => '',
+	'priority' => 'bottom',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'author_company' => 'Marketing Factory Consulting GmbH',
+	'version' => '0.0.2',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '5.3.0-0.0.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => '',
+);
+
+?>
