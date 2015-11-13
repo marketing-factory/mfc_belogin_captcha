ec4e48851c6a4bad96d4a4a662d612fe0531e37f
alex_schnitzler <alex_schnitzler@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-11-13 11:59:19 +0000

    [TASK] Remove TYPO3 4 related source code
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mfc_belogin_captcha/trunk@90212 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_autoload.php
+++ ext_autoload.php
@@ -3,9 +3,6 @@
 $extensionPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('mfc_belogin_captcha');
 
 return [
-    'tx_mfcbelogincaptcha_captchaservice' => $extensionPath . '/Classes/Service/CaptchaService.4x.php',
-    'tx_mfcbelogincaptcha_settingsservice' => $extensionPath . '/Classes/Service/SettingsService.4x.php',
-
     'ux_sc_index' => $extensionPath . '/Classes/XClass/BackendLogin.php',
     'ux_t3lib_beuserauth' => $extensionPath . '/Classes/XClass/BeUserAuth.php',
 ]
