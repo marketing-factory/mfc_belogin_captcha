bafbd672cccc422237a2be9158ee89a61dc7672d
Sebastian Fischer <sf@marketing-factory.de>
2016-12-15 08:01:21 +0000

    Cleanup

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,36 +1,20 @@
 <?php
-/** @var string $_EXTKEY */
 
-$EM_CONF[$_EXTKEY] = [
+$EM_CONF['mfc_belogin_captcha'] = [
     'title' => 'Backend Login Captcha',
     'description' => 'Add an configurable captcha to the backend login after a give amount of failed login tries',
-    'category' => 'be',
+    'version' => '2.0.1',
     'author' => 'Sebastian Fischer',
     'author_email' => 'typo3@marketing-factory.de',
-    'shy' => '',
-    'conflicts' => '',
-    'priority' => 'bottom',
-    'module' => '',
-    'state' => 'beta',
-    'internal' => '',
-    'uploadfolder' => 0,
-    'createDirs' => '',
-    'modify_tables' => '',
-    'clearCacheOnLoad' => 1,
-    'lockType' => '',
     'author_company' => 'Marketing Factory Consulting GmbH',
-    'version' => '2.0.1',
+    'category' => 'be',
+    'priority' => 'bottom',
+    'state' => 'stable',
+    'clearCacheOnLoad' => 1,
     'constraints' => [
         'depends' => [
             'typo3' => '7.6.0-7.6.99',
             'recaptcha' => ''
         ],
-        'conflicts' => [
-        ],
-        'suggests' => [
-        ],
     ],
-    'suggests' => [
-    ],
-    '_md5_values_when_last_written' => '',
 ];
