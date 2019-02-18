b9bff0306ac555367208ac5fdf4c6f056ae81004
Christian Hellmund <ch@marketing-factory.de>
2019-02-18 14:11:04 +0100

    Set correct authors

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,8 +3,8 @@
 $EM_CONF['mfc_belogin_captcha'] = [
     'title' => 'Backend Login Captcha',
     'description' => 'Add an configurable captcha to the backend login after a give amount of failed login tries',
-    'version' => '2.1.1',
-    'author' => 'Sebastian Fischer',
+    'version' => '2.2.0',
+    'author' => 'Simon Schmidt, Christian Hellmund, Alexander Schnitzler',
     'author_email' => 'typo3@marketing-factory.de',
     'author_company' => 'Marketing Factory Consulting GmbH',
     'category' => 'be',
