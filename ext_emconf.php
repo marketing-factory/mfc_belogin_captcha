0ac0f2c324d8e7d433f9dba24dcced784cc18799
Christian Hellmund <ch@marketing-factory.de>
2019-02-18 14:43:18 +0100

    Set correct authors. Add requirement for evoweb/receptcha on composer usage

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,8 +3,8 @@
 $EM_CONF['mfc_belogin_captcha'] = [
     'title' => 'Backend Login Captcha',
     'description' => 'Add an configurable captcha to the backend login after a give amount of failed login tries',
-    'version' => '3.0.0',
-    'author' => 'Sebastian Fischer',
+    'version' => '4.0.0',
+    'author' => 'Simon Schmidt, Christian Hellmund, Alexander Schnitzler',
     'author_email' => 'typo3@marketing-factory.de',
     'author_company' => 'Marketing Factory Consulting GmbH',
     'category' => 'be',
